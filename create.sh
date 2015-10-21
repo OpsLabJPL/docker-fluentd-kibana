8ca9e4635a0cde5beb79edd2e7d6ac206570555b
Tom Crockett <pelotom@gmail.com>
2015-10-21 19:55:21 +0000

    Build and start scripts

--- create.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run --name=holometry --restart=always -d -p 9200:9200 -p 9300:9300 -p 24224:24224 -p 8888:8888 -p 80:80 -v /data:/data pelotom/docker-fluentd-kibana
--- destroy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker stop holometry && docker rm holometry
